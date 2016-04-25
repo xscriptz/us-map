f076ab0374144109e3611ed9dd700b8f04ab192f
xscriptz <athallur@iu.edu>
2016-04-25 11:27:07 -0400

    Data Set changed to a single population, lat, long file

---
+++ WebContent/smallData.js
@@ -0,0 +1,33 @@
+var finalDataArray = [{
+	"Longitude": "-88.075733",
+	"Zipcode": "60002",
+	"ZipClass": "STANDARD",
+	"County": "LAKE",
+	"City": "ANTIOCH",
+	"State": "IL",
+	"Latitude": "42.451419",
+	"Population": "24299",
+	"data": [{
+		"Label": "MalePopulation",
+		"value": "12213"
+	}, {
+		"Label": "FemalePopulation",
+		"value": "12086"
+	}]
+}, {
+	"Longitude": "-85.113166",
+	"Zipcode": "46747",
+	"ZipClass": "STANDARD",
+	"County": "STEUBEN",
+	"City": "HUDSON",
+	"State": "IN",
+	"Population": "2693",
+	"Latitude": "41.577125",
+	"data": [{
+		"Label": "MalePopulation",
+		"value": "1386"
+	}, {
+		"Label": "FemalePopulation",
+		"value": "1307"
+	}]
+}];
